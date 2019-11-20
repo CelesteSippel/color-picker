e246adf6d172a250a1513e82499bab1f478e4658
Celeste Sippel <celestechristine@gmail.com>
2019-11-20 09:12:36 -0500

    changed the useState back to 0

--- src/components/ChangeColor.js
+++ src/components/ChangeColor.js
@@ -4,20 +4,23 @@ const ChangeColor = () => {
   const [hue, setHue] = useState(Math.floor(Math.random() * 360))
   const [saturation, setSaturation] = useState(0)
   const [lightness, setLightness] = useState(0)
+  const [alpha, setAlpha] = useState(Math.floor(Math.random() * 100))
   return (
     <main>
       <section className="color-picker-display">
         <section
           className="background-box"
           style={{
-            backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness}%)`
+            backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness}%, ${alpha})`
           }}
         >
           <h2
             className="hsl-info"
-            style={{ color: `hsl(${hue}, ${saturation}, ${lightness})` }}
+            style={{
+              color: `hsl(${hue}, ${saturation}, ${lightness}, ${alpha})`
+            }}
           >
-            {`hsl(${hue}, ${saturation}%, ${lightness}%)`}
+            {`hsl(${hue}, ${saturation}%, ${lightness}%, ${alpha})`}
           </h2>
         </section>
       </section>
@@ -47,11 +50,20 @@ const ChangeColor = () => {
         }}
       />
       <section className="slider-type">Lightness</section>
+      <input
+        type="range"
+        defaultValue={alpha}
+        onChange={e => {
+          setAlpha(e.target.value)
+        }}
+      />
+      <section className="slider-type">Alpha</section>
       <button
         onClick={() => {
           setHue(Math.floor(Math.random() * 360))
           setSaturation(Math.floor(Math.random() * 100))
           setLightness(Math.floor(Math.random() * 100))
+          setAlpha(Math.floor(Math.random() * 100))
         }}
       >
         Random Color
